304757453c213085166e1b38b326ac0774c4f9b3
JustSteve2k <srodriguez2k@gmail.com>
2022-10-25 21:36:27 -0500

    add in db connection

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const URI = "mongodb+srv://admin:mongopassword@cluster0.aud2amv.mongodb.net/quotes";
+    // "mongodb+srv://admin:mongopassword@cluster0.aud2amv.mongodb.net/?retryWrites=true&w=majority";
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(URI);
+
+    console.log(`MongoDB Connected ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
