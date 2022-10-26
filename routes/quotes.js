74f0996c4c071763b7ea6a3c652d8401e259cc4d
JustSteve2k <srodriguez2k@gmail.com>
2022-10-26 16:24:05 -0500

    add futurama get post and delete routes

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Simpsons = require("../models/simpsonsModel");
+const Futurama = require("../models/futuramaModel");
 
 const router = express.Router();
 
@@ -17,7 +18,7 @@ router.post("/simpsons", async (req, res, next) => {
   let episode = req.body.episode;
   let extra = req.body.extra;
 
-  console.log(quote);
+  console.log(quote.green);
   console.log(speaker);
   console.log(episode);
   console.log(extra);
@@ -33,8 +34,58 @@ router.post("/simpsons", async (req, res, next) => {
   res.send("Thanks for sending the request!");
 });
 
-router.get("/futurama", (req, res, next) => {
-  res.send("<h1>this will produce a random futurama quote</h1>");
+router.get("/futurama", async (req, res, next) => {
+  let quote = "";
+
+  Futurama.count().exec((err, count) => {
+    // Get a random entry
+    let random = Math.floor(Math.random() * count);
+
+    // Again query all users but only fetch one offset by our random #
+    Futurama.findOne()
+      .skip(random)
+      .exec(function (err, result) {
+        console.log(result);
+        res.send(result);
+        //res.send(`${result.quote} - ${result.speaker}`);
+      });
+  });
 });
 
+router.post("/futurama", async (req, res, next) => {
+  let quote = req.body.quote;
+  let speaker = req.body.speaker;
+  let episode = req.body.episode;
+
+  let saying = { quote: quote, speaker: speaker, episode: episode };
+  console.log(saying);
+  // res.send(`Got the saying from ${speaker}`);
+
+  const result = await Futurama.create(saying);
+  res.send(result);
+  console.log(result);
+});
+
+router.post("/futurama/person", async (req, res, next) => {
+  let name = req.query.name;
+
+  console.log(name);
+  console.log(`Looking for ${name}`);
+  const result = await Futurama.find({ speaker: name });
+
+  console.log(result);
+  console.log(`Found ${result.length} matches`);
+
+  result.length > 0 ? res.send(` ${name}, found ${result.length} matches`) : res.send("Couldnt find anything with that info.");
+});
+
+router.delete("/futurama/person", async (req, res, next) => {
+  let name = req.query.name;
+
+  const result = await Futurama.deleteOne({ speaker: name });
+
+  console.log(result);
+
+  res.send(`You want to delete entry with ${name} as the name , ${result.deletedCount} were found and deleted`);
+});
 module.exports = router;
