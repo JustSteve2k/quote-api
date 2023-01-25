bcf3c155a83292ac5c1a121a266c7ba34dea4c1f
JustSteve2k <srodriguez2k@gmail.com>
2023-01-24 23:32:11 -0600

    move logic to controller

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,38 +1,16 @@
 const express = require("express");
-const Simpsons = require("../models/simpsonsModel");
+const quotesController = require("../controllers/quotesController");
 const Futurama = require("../models/futuramaModel");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("<h1>Here is the main page for quotes</h1>.");
-});
+router.get("/", quotesController.getSimpsons);
 
 router.get("/simpsons", (req, res, next) => {
   res.send("<h1>this will produce a random simpsons quote</h1>");
 });
 
-router.post("/simpsons", async (req, res, next) => {
-  let quote = req.body.quote;
-  let speaker = req.body.speaker;
-  let episode = req.body.episode;
-  let extra = req.body.extra;
-
-  console.log(quote.green);
-  console.log(speaker);
-  console.log(episode);
-  console.log(extra);
-
-  let saying = { quote: quote, speaker: speaker, episode: episode, extra: extra };
-
-  console.log(saying);
-
-  const result = await Simpsons.create(saying);
-
-  console.log(result);
-
-  res.send("Thanks for sending the request!");
-});
+router.post("/simpsons", quotesController.postSimpsons);
 
 router.get("/futurama", async (req, res, next) => {
   let quote = "";
