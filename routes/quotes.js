1cd2a67feccf88517eba77a7d4309d78b1da4323
JustSteve2k <srodriguez2k@gmail.com>
2023-01-30 16:16:28 -0600

    move logic to controller, fix names

--- routes/quotes.js
+++ routes/quotes.js
@@ -4,44 +4,22 @@ const Futurama = require("../models/futuramaModel");
 
 const router = express.Router();
 
-router.get("/", quotesController.getSimpsons);
+router.get("/", quotesController.getQuotesMain);
 
-router.get("/simpsons", (req, res, next) => {
-  res.send("<h1>this will produce a random simpsons quote</h1>");
-});
+router.get("/simpsons", quotesController.getSimpsonsRandom);
 
 router.post("/simpsons", quotesController.postSimpsons);
 
-router.get("/futurama", async (req, res, next) => {
-  let quote = "";
-
-  Futurama.count().exec((err, count) => {
-    // Get a random entry
-    let random = Math.floor(Math.random() * count);
-
-    // Again query all users but only fetch one offset by our random #
-    Futurama.findOne()
-      .skip(random)
-      .exec(function (err, result) {
-        console.log(result);
-        res.send(result);
-        //res.send(`${result.quote} - ${result.speaker}`);
-      });
-  });
-});
+router.get("/futurama", quotesController.getFuturama);
 
 router.post("/futurama", quotesController.postFuturama);
 
-router.get("/Futurama/person", quotesController.getFuturamaPerson);
+router.get("/futurama/person", quotesController.getFuturamaPerson);
 
-router.delete("/futurama/person", async (req, res, next) => {
-  let name = req.query.name;
+router.delete("/futurama/person", quotesController.deleteFuturamaPerson);
 
-  const result = await Futurama.deleteOne({ speaker: name });
-
-  console.log(result);
-
-  res.send(`You want to delete entry with ${name} as the name , ${result.deletedCount} were found and deleted`);
-});
+// Needs
+// simpsons/count - tells how many quotes are in the database for simpsons
+// futurama/count - tells how many quotes are in the databse for futurama
 
 module.exports = router;
